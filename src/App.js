73da7c260ed22e623d66e0698a6ca8776e43b272
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-12 14:36:02 +0100

    LE 321 add const

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useGlobalContext } from "./context";
 import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
-// 228/229/
+// 228/229/230/231/
 function App() {
   const { waiting, loading, questions, index, correct } = useGlobalContext();
   if (waiting) {
@@ -13,7 +13,34 @@ function App() {
   if (loading) {
     return <Loading />;
   }
-  return <main>quiz app</main>;
+  // console.log(questions);
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers :{correct}/{index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+        </article>
+        <div className="btn-container">
+          {answers.map((answer, index) => {
+            return (
+              <button
+                key={index}
+                className="answer-btn"
+                dangerouslySetInnerHTML={{ __html: answer }}
+              />
+            );
+          })}
+        </div>
+        <button className="next-question">next questions</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
