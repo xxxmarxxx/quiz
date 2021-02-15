82388cd8f02b60f9eb645b434dcb1c23741e3528
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-15 13:50:35 +0100

    change

--- src/Modal.js
+++ src/Modal.js
@@ -6,12 +6,17 @@ const Modal = () => {
   // const { isModalOpen, closeModal, correct, questions } = useGlobalContext()
   return (
     <div
-      className={`${isModalOpen ? "modal-container isOpen" : "modal-container"}`}
+      className={`${
+        isModalOpen ? "modal-container isOpen" : "modal-container"
+      }`}
     >
       <div className="modal-content">
         <h2>congrats!</h2>
-        <p>You answered {((correct/questions.length)*100).toFixed(0)}% of questions correctly</p>
-        <button className='close-btn' onClick={closeModal}>
+        <p>
+          You answered {((correct / questions.length) * 100).toFixed(0)}% of
+          questions correctly
+        </p>
+        <button className="close-btn" onClick={closeModal}>
           play again
         </button>
       </div>
